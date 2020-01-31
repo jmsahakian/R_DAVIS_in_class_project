0cd5723f8b0d225996d4b5d3252b06b58630d0ed
Jessie Sahakian <jsahakian@ae.local>
2020-01-31 18:01:07 -0500

    homework

---
+++ code/homework_week4.R
@@ -0,0 +1,21 @@
+#Week 4 homework 
+
+
+#Problem 1
+surveys <- read.csv("data/raw_data/portal_data_joined.csv")
+
+surveys_base <- surveys[c("species_id","plot_type","weight")]
+
+surveys_base <- surveys_base[1:60,]
+    
+surveys_base$species_id <- (as.character(surveys_base$species_id))
+surveys_base$plot_type <- (as.character(surveys_base$plot_type))
+
+surveys_base <- surveys_base[!is.na.data.frame(surveys_base)]
+
+challenge_base <- surveys_base
+
+surveys_base$weight > 150
+
+
+#Problem 2
